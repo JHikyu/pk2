fb368b1495fe63cc3923355b52df24fe1119da68
JHikyu <therosti@gmail.com>
2023-06-09 12:19:32 +0200

    LinkedList Done

--- c++/woche 7/praktikum/LinkedList.cpp
+++ c++/woche 7/praktikum/LinkedList.cpp
@@ -2,11 +2,11 @@
 
 LinkedList::LinkedList() { };
 int LinkedList::append(const char* text) {
-  Item newItem;
-  newItem.value = text;
+  Item* newItem = new Item;
+  newItem->value = text;
 
   if(firstItem == nullptr) {
-    firstItem = &newItem;
+    firstItem = newItem;
     return 1;
   }
 
@@ -15,15 +15,104 @@ int LinkedList::append(const char* text) {
   while(currentItem->next != nullptr) currentItem = currentItem->next;
 
   // Add next
-  currentItem->next = &newItem;
+  currentItem->next = newItem;
 
   return 1;
 }
 
-// int insert(const char* text, int p);
-// int remove(int p);
-// const char* get(int p);
-// int index_of(const char* text);
-// const char* first();
-// const char* last();
-// void visit_all(void (*work)(const char* t));
+int LinkedList::insert(const char* text, int p) {
+  if(p < 0) return 0;
+
+  Item* newItem = new Item;
+  newItem->value = text;
+
+  // If list empty and p == 0, set first element
+  if(firstItem == nullptr) {
+    if(p != 0) return 0;
+
+    firstItem = newItem;
+    return 1;
+  }
+
+  Item* currentItem = firstItem;
+  while(currentItem->next != nullptr) {
+    currentItem = currentItem->next;
+    p--;
+  }
+
+  if(p > 0) return 0;
+
+  currentItem->next = newItem;
+  return 1;
+}
+
+int LinkedList::remove(int p) {
+  Item* currentItem = firstItem;
+  while(currentItem->next != nullptr) {
+    currentItem = currentItem->next;
+    if(--p == 1) break;
+  }
+
+  if(p > 1) return 0;
+
+  currentItem->next = currentItem->next->next;
+  return 1;
+}
+
+const char* LinkedList::get(int p) {
+  if(p < 0) return "";
+  if(firstItem == nullptr) return "";
+
+  Item* currentItem = firstItem;
+  while(currentItem->next != nullptr) {
+    currentItem = currentItem->next;
+    if(--p == 0) break;
+  }
+  if(p > 0) return 0;
+
+  return currentItem->value;
+}
+
+int LinkedList::index_of(const char* text) {
+  if(firstItem == nullptr) return -1;
+
+  int i = 0;
+
+  if(firstItem->value == text) return i;
+
+  Item* currentItem = firstItem->next;
+  while(currentItem->next != nullptr) {
+    currentItem = currentItem->next;
+    i++;
+    if(currentItem->value == text) return i;
+  }
+
+  return 0;
+}
+
+const char* LinkedList::first() {
+  return firstItem->value;
+}
+
+const char* LinkedList::last() {
+  if(firstItem == nullptr) return "";
+
+  Item* currentItem = firstItem->next;
+  while(currentItem->next != nullptr) {
+    currentItem = currentItem->next;
+  }
+
+  return currentItem->value;
+}
+
+void LinkedList::visit_all(void(*work)(const char* t)) {
+  if(firstItem == nullptr) return;
+
+  Item* currentItem = firstItem->next;
+  work(currentItem->value);
+
+  while(currentItem->next != nullptr) {
+    currentItem = currentItem->next;
+    work(currentItem->value);
+  }
+}
--- c++/woche 7/praktikum/main.cpp
+++ c++/woche 7/praktikum/main.cpp
@@ -1,3 +1,5 @@
+#include "LinkedList.h"
+
 int main() {
 
   return 0;
